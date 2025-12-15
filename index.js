ade7fd12b742fb190959d81fda19d304191436f2
Jahid <jahidict56@gmail.com>
2025-12-15 12:58:55 +0600

    get API and connect mongodb

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3xhvrfl.mongodb.net/?appName=Cluster0` ;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run(){
+    try{
+        const db = client.db("City-Pulse-DB");
+        const issuesCollection = db.collection("Issues");
+
+         app.get("/issues",async(req,res)=>{
+        const result = await issuesCollection.find().toArray()
+        res.send(result)  
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get("/",(req,res)=>{
     res.send("Issue server is running now...");
