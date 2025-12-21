40897aeb55cccaf868f37e245e9299dd3a14191b
Jahid <jahidict56@gmail.com>
2025-12-22 02:41:07 +0600

    post api for users

--- index.js
+++ index.js
@@ -6,8 +6,42 @@ const app = express();
 const port = process.env.PORT || 3000;
 const stripe = require('stripe')(process.env.STRIPE);
 
+const admin = require("firebase-admin");
+const serviceAccount = require("./citypulse-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+function generateTrackingId() {
+  const prefix = "CP";
+  const date = new Date().toISOString().slice(0,10).replace(/-/g, "");
+  const random = Math.random().toString(36).substring(2, 8).toUpperCase();
+
+  return `${prefix}-${date}-${random}`;
+}
+
 app.use(cors());
 app.use(express.json());
+
+const verifyFbToken = async(req, res,next)=>{
+  const token = req.headers.authorization;
+
+  if(!token){
+    return res.status(401).send({message: "unauthorize access"})
+  }
+
+  try{
+    const idToken = token.split(' ')[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    req.decoded_email = decoded.email;
+    next()
+  }
+  catch(err){
+    res.status(403).send({message:"unauthorize access"})
+  }
+  
+}
 
 const uri =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3xhvrfl.mongodb.net/?appName=Cluster0` ;
 const client = new MongoClient(uri, {
@@ -22,8 +56,18 @@ async function run(){
     try{
         const db = client.db("City-Pulse-DB");
         const issuesCollection = db.collection("Issues");
+        const userCollection = db.collection("users");
         const reviewsCollection = db.collection("Reviews");
         const paymentCollection = db.collection('payments');
+
+        app.post('/users',async(req,res)=>{
+          const user = req.body;
+          user.role = 'user';
+          user.createdAt = new Date();
+
+          const result = await userCollection.insertOne(user);
+          res.send(result);
+        })
 
         app.get("/issues",async(req,res)=>{
         const result = await issuesCollection.find().sort({priority:1}).toArray()
@@ -114,14 +158,23 @@ async function run(){
 
       app.patch('/payment-success', async(req ,res)=>{
         const sessionId = req.query.session_id;
-
         const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+        const transactionId = session.payment_intent;
+        const query = {transactionId: transactionId}
+        const paymentExist = await paymentCollection.findOne(query)
+        if(paymentExist){
+          return res.send({message: "Already exist ", transactionId})
+        }
+
+        const trackingId = generateTrackingId()
         if(session.payment_status === 'paid'){
           const id = session.metadata.issueId;
           const query = {_id: new ObjectId(id)}
           const update ={
             $set :{
               priority: 'High',
+              trackingId: trackingId,
             }
           }
           const result = await issuesCollection.updateOne(query ,update);
@@ -135,17 +188,37 @@ async function run(){
             transactionId: session.payment_intent,
             paymentStatus: session.payment_status,
             paidAt: new Date(),
-            trackingId:'',
           }
 
           if(session.payment_status === "paid"){
-              const resultPamyment = await paymentCollection.insertOne(payment);
-              res.send({success: true, modifyIssue: result, paymentInfo: resultPamyment })
+              const resultPayment = await paymentCollection.insertOne(payment);
+              res.send({success: true,
+                 modifyIssue: result, 
+                 paymentInfo: resultPayment,
+                 trackingId:trackingId ,
+                 transactionId:session.payment_intent
+                })
           }
 
         }
 
         res.send({success: false})
+      })
+
+      app.get('/payments', verifyFbToken, async(req,res)=>{
+        const email = req.query.email
+        const query = {}
+
+        if(email){
+          query.customerEmail = email;
+
+            if(email !== req.decoded_email){
+              return res.status(403).send({message: 'forbidden access'})
+            }
+        }
+        const cursor = paymentCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
       })
 
 
